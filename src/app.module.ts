70b3c21e96ec4ec32a57427d76f75e16f4540663
wyn <thang09052004@gmail.com>
2025-05-22 18:20:30 +0700

    config redisdb

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,10 @@ import { TypeOrmConfigService } from '@database/config/type-orm.config';
 import mongoConfig from '@database/config/mongo.config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FeedbackModule } from '@modules/feedback/feedback.module';
+import redisConfig from '@database/config/redis.config';
+import { RedisModule, RedisModuleOptions } from '@nestjs-modules/ioredis';
+import * as redisStore from 'cache-manager-ioredis';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
@@ -19,6 +23,7 @@ import { FeedbackModule } from '@modules/feedback/feedback.module';
         appConfig,
         postgreConfig,
         mongoConfig,
+        redisConfig,
         // authConfig,
         // mailConfig,
         // fileConfig,
@@ -40,6 +45,33 @@ import { FeedbackModule } from '@modules/feedback/feedback.module';
         uri: configService.get<string>('mongo.uri'),
       }),
       inject: [ConfigService],
+    }),
+    RedisModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): RedisModuleOptions => ({
+        type: 'single',
+        options: {
+          host: configService.get<string>('redis.host'),
+          port: configService.get<number>('redis.port'),
+          password: configService.get<string>('redis.password'),
+        },
+      }),
+    }),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      isGlobal: true,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const redisConfig = configService.get('redis');
+        return {
+          store: redisStore,
+          host: redisConfig.host,
+          port: redisConfig.port,
+          password: redisConfig.password,
+          ttl: 300,
+        };
+      },
     }),
     UsersModule,
     FeedbackModule,
--- src/configs/config.type.ts
+++ src/configs/config.type.ts
@@ -1,9 +1,11 @@
 import { AppConfig } from 'src/types/app-config.type';
 import { MongoConfig } from 'src/types/mongo-config.type';
 import { PostgresConfig } from 'src/types/postgre-config.type';
+import { RedisConfig } from 'src/types/redis-config.type';
 
 export type AllConfigType = {
   app: AppConfig;
   postgres: PostgresConfig;
   mongo: MongoConfig;
+  redis: RedisConfig;
 };
